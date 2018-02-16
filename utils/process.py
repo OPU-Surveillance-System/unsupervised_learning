4c8fc80ea5b566d85c5327afef1c4a7813664ebb
henriojord <henriojord@eisti.eu>
2018-02-16 11:41:25 +0900

    add a set of data processing functions

---
+++ utils/process.py
@@ -0,0 +1,21 @@
+def preprocess(x):
+    """
+    Normalize the given image between -1 and 1
+    Args:
+        x (numpy.array or torch.Tensor): Image of any shape (assume pixels to range in [0, 255])
+    """
+
+    x = (x - 127.5) / 127.5
+
+    return x
+
+def deprocess(x):
+    """
+    Denormalize the given normalized image between 0 and 1
+    Args:
+        x (numpy.array or torch.Tensor): Image of any shape (assume pixels to range in [-1, 1])
+    """
+
+    x = (x + 1) * 0.5
+
+    return x
