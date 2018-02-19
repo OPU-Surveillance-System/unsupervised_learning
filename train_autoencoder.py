be19eb90da1af737383e4d597a20c9d5d24cfff9
henriojord <henriojord@eisti.eu>
2018-02-19 14:17:38 +0900

    swap files

--- train_autoencoder.py
+++ train_autoencoder.py
@@ -15,24 +15,22 @@ import utils.metrics
 import utils.plot
 import utils.process
 
-def train(model, loss_function, optimizer, n_trainset, a_trainset, testset, negative_weight, epoch, batch_size, directory):
+def train(model, loss_function, optimizer, trainset, testset, epoch, batch_size, directory):
     """
     Train a model and log the process
     Args:
         model (torch.nn.Module): Model to train (autoencoder)
         loss_function (torch.optim.Module): Loss function
         optimizer (torch.optim.Optimizer): Optimizer
-        n_trainset (torch.utils.data.Dataset): Training set of normal patterns
-        a_trainset (torch.utils.data.Dataset): Training set of abnormal patterns
+        trainset (torch.utils.data.Dataset): Training set
         testset (torch.utils.data.Dataset): Test set
-        negative_weight (float): Weight for the negative loss
         epoch (int): Number of training epochs
         batch_size (int): Mini batch size
         directory (str): Directory to store the logs
     """
 
-    phase = ('n_train', 'a_train', 'test')
-    datasets = {'n_train': n_trainset, 'a_train': a_trainset, 'test': testset}
+    phase = ('train', 'test')
+    datasets = {'train': trainset, 'test': testset}
     dist = torch.nn.PairwiseDistance(p=2, eps=1e-06)
     best_auc = 0
     best_model = copy.deepcopy(model)
@@ -41,7 +39,7 @@ def train(model, loss_function, optimizer, n_trainset, a_trainset, testset, nega
     for e in range(epoch):
         print('Epoch {}'.format(e))
         for p in phase:
-            if p == 'n_train' or p == 'a_train':
+            if p == 'train':
                 model.train()
             else:
                 labels = []
@@ -54,9 +52,7 @@ def train(model, loss_function, optimizer, n_trainset, a_trainset, testset, nega
                 inputs = Variable(sample['img'].float().cuda())
                 logits, pred = model(inputs)
                 loss = loss_function(logits, inputs)
-                if p == 'n_train' or p == 'a_train':
-                    if p == 'a_train':
-                        loss = - negative_weight * loss
+                if p == 'train':
                     loss.backward()
                     optimizer.step()
                 running_loss += loss.data[0]
@@ -121,12 +117,11 @@ def main(args):
     loss_function = torch.nn.MSELoss()
     optimizer = torch.optim.Adam(ae.parameters(), args.learning_rate)
 
-    n_trainset = dataset.VideoDataset(args.normal_trainset, args.root_dir)
-    a_trainset = dataset.VideoDataset(args.abnormal_trainset, args.root_dir)
+    trainset = dataset.VideoDataset(args.trainset, args.root_dir)
     testset = dataset.VideoDataset(args.testset, args.root_dir)
 
     #Train the model and save it
-    best_model = train(ae, loss_function, optimizer, n_trainset, a_trainset, testset, args.negative_weight, args.epoch, args.batch_size, args.directory)
+    best_model = train(ae, loss_function, optimizer, trainset, testset, args.epoch, args.batch_size, args.directory)
     torch.save(best_model.state_dict(), os.path.join(args.directory, 'serial', 'best_model'))
 
     return 0
@@ -134,13 +129,11 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='')
     #Training arguments
-    parser.add_argument('--trsn', dest='normal_trainset', type=str, default='data/umn_normal_trainset', help='Path to the trainset summary of normal patterns')
-    parser.add_argument('--trsa', dest='abnormal_trainset', type=str, default='data/umn_abnormal_trainset', help='Path to the trainset summary of abnormal patterns')
-    parser.add_argument('--tes', dest='testset', type=str, default='data/umn_testset', help='Path to the testset summary')
+    parser.add_argument('--trs', dest='trainset', type=str, default='data/umn/trainset', help='Path to the trainset summary')
+    parser.add_argument('--tes', dest='testset', type=str, default='data/umn/testset', help='Path to the testset summary')
     parser.add_argument('--rd', dest='root_dir', type=str, default='/datasets', help='Path to the images')
     parser.add_argument('--bs', dest='batch_size', type=int, default=16, help='Mini batch size')
     parser.add_argument('--lr', dest='learning_rate', type=float, default=0.0001, help='Learning rate')
-    parser.add_argument('--nw', dest='negative_weight', type=float, default=0.1, help='Negative loss weight')
     parser.add_argument('--ep', dest='epoch', type=int, default=100, help='Number of training epochs')
     parser.add_argument('--dir', dest='directory', type=str, default='train_autoencoder', help='Directory to store results')
     #Model arguments
