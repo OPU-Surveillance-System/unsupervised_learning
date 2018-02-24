df8e0e880512f120021b0a9f3f680a4b20f0ac54
henriojord <henriojord@gmail.com>
2018-02-24 10:34:23 +0900

    add patch wise autoencoder

---
+++ models/patch_autoencoder.py
@@ -0,0 +1,91 @@
+import torch
+import math
+
+class Autoencoder(torch.nn.Module):
+    def __init__(self, nb_f, nb_l, nb_b, fc=None, random_projection=False, input_size=256, patch=32):
+        super(Autoencoder, self).__init__()
+        self.in_dim = 3
+        self.nb_f = nb_f
+        self.nb_l = nb_l
+        self.nb_b = nb_b
+        self.fc = fc
+        self.random_projection = random_projection
+        self.ips = input_size
+        self.patch = patch
+
+        def downsampling_block(in_dim, nb_f, nb_l, rate):
+            layers = []
+            for n in range(nb_l):
+                layers.append(torch.nn.Conv2d(in_dim, nb_f, (3, 3), padding=1))
+                layers.append(torch.nn.SELU())
+                in_dim = nb_f
+            layers.append(torch.nn.MaxPool2d((2, 2), (2, 2)))
+
+            return layers
+
+        def upsampling_block(in_dim, nb_f, nb_l, rate):
+            layers = [torch.nn.ConvTranspose2d(in_dim, nb_f, (2, 2), (2, 2))]
+            layers.append(torch.nn.SELU())
+            for n in range(nb_l):
+                layers.append(torch.nn.Conv2d(nb_f, nb_f, (3, 3), padding=1))
+                layers.append(torch.nn.SELU())
+
+            return layers
+
+        def build_bottleneck(in_dim, h_dim, rate):
+            layers = [torch.nn.Linear(in_dim, h_dim)]
+            layers.append(torch.nn.Linear(h_dim, in_dim))
+
+            return layers
+
+        #Encoder
+        layers = []
+        prev_in = self.in_dim
+        prev_f = self.nb_f
+        for n in range(self.nb_b):
+            layers += downsampling_block(prev_in, prev_f, self.nb_l, self.rate)
+            prev_in = prev_f
+            prev_f *= 2
+        self.encoder = torch.nn.Sequential(*layers)
+
+        #Bottleneck
+        if self.fc:
+            in_dim = ((self.shape//(2**self.nb_b))**2)*(prev_f//2) #last_feature_map.h * last_feature_map.w * last_feature_map.c
+            layers = build_bottleneck(in_dim, self.fc, self.rate)
+            self.bottleneck = torch.nn.Sequential(*layers)
+
+        #Decoder
+        layers = []
+        for n in range(self.nb_b):
+            prev_f //= 2
+            next_f = prev_f // 2
+            layers += upsampling_block(prev_f, next_f, self.nb_l, self.rate)
+        layers.append(torch.nn.Conv2d(next_f, self.in_dim, (3, 3), padding=1))
+        self.decoder = torch.nn.Sequential(*layers)
+
+        #Weights initialization
+        for m in self.modules():
+            if isinstance(m, torch.nn.Conv2d) or isinstance(m, torch.nn.ConvTranspose2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2.)*math.sqrt(2. / n))
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, torch.nn.Linear):
+                m.weight.data.normal_(0, math.sqrt(2. / m.in_features))
+                m.bias.data.zero_()
+
+        if self.random_projection:
+            for p in self.bottleneck.parameters():
+                p.requires_grad = False
+
+    def forward(self, x):
+        x = x.view(-1, 3, self.patch, self.patch)
+        x = self.encoder(x)
+        if self.fc:
+            x = x.view(x.size(0), -1)
+            x = self.bottleneck(x)
+            reshape = self.patch//(2**self.nb_b)
+            x = x.view(x.size(0), -1, reshape, reshape)
+        logits = self.decoder(x)
+
+        return logits
---
+++ train_patch_autoencoder.py
@@ -0,0 +1,159 @@
+import argparse
+import os
+import torch
+import copy
+import numpy as np
+from torch.utils.data import DataLoader
+from torch.autograd import Variable
+from tqdm import tqdm
+from sklearn import metrics
+from tensorboardX import SummaryWriter
+
+import dataset
+import models.autoencoder
+import utils.metrics
+import utils.plot
+import utils.process
+
+def train(model, loss_function, optimizer, trainset, testset, epoch, batch_size, reg, directory):
+    """
+    Train a model and log the process
+    Args:
+        model (torch.nn.Module): Model to train (autoencoder)
+        loss_function (torch.optim.Module): Loss function
+        optimizer (torch.optim.Optimizer): Optimizer
+        trainset (torch.utils.data.Dataset): Training set
+        testset (torch.utils.data.Dataset): Test set
+        epoch (int): Number of training epochs
+        batch_size (int): Mini batch size
+        directory (str): Directory to store the logs
+    """
+
+    phase = ('train', 'test')
+    datasets = {'train': trainset, 'test': testset}
+    dist = torch.nn.PairwiseDistance(p=2, eps=1e-06)
+    best_auc = 0
+    best_model = copy.deepcopy(model)
+    writer = SummaryWriter(os.path.join(directory, 'logs'))
+
+    for e in range(epoch):
+        print('Epoch {}'.format(e))
+        for p in phase:
+            if p == 'train':
+                model.train()
+            else:
+                labels = []
+                errors = []
+                model.eval()
+            running_loss = 0
+            dataloader = DataLoader(datasets[p], batch_size=batch_size, shuffle=True, num_workers=4)
+            for i_batch, sample in enumerate(tqdm(dataloader)):
+                model.zero_grad()
+                inputs = Variable(sample['img'].float().cuda())
+                logits, pred = model(inputs)
+                loss = loss_function(logits, inputs.view(-1, 3, args.patch, args.patch)) #- reg * torch.mean(torch.norm(logits.view(-1, logits.size(1) * logits.size(2) * logits.size(3)), 2, 1))
+                if p == 'train':
+                    loss.backward()
+                    optimizer.step()
+                running_loss += loss.data[0]
+                if p == 'test':
+                    logits = logits.view(-1, 3, args.ips, args.ips)
+                    tmp = utils.metrics.per_image_error(dist, logits, inputs)
+                    errors += tmp.data.cpu().numpy().tolist()
+                    labels += sample['lbl'].numpy().tolist()
+
+            if p == 'test':
+                fpr, tpr, thresholds = metrics.roc_curve(labels, errors)
+                auc = metrics.auc(fpr, tpr)
+            else:
+                auc = 0
+            epoch_loss = running_loss / len(datasets[p])
+            writer.add_scalar('learning_curve/{}'.format(p), epoch_loss, e)
+            print('{} -- Loss: {} AUC: {}'.format(p, epoch_loss, auc))
+            if p == 'test':
+                writer.add_scalar('auc', auc, e)
+                if auc > best_auc:
+                    best_auc = auc
+                    best_model = copy.deepcopy(model)
+                if e % 10 == 0:
+                    #Save model
+                    torch.save(model.state_dict(), os.path.join(directory, 'serial', 'model_{}'.format(e)))
+
+                    #Plot example of reconstructed images
+                    pred = utils.process.deprocess(logits)
+                    pred = pred.data.cpu().numpy()
+                    pred = np.rollaxis(pred, 1, 4)
+                    inputs = utils.process.deprocess(inputs)
+                    inputs = inputs.data.cpu().numpy()
+                    inputs = np.rollaxis(inputs, 1, 4)
+                    utils.plot.plot_reconstruction_images(inputs, pred, os.path.join(directory, 'example_reconstruction', 'epoch_{}.svg'.format(e)))
+    writer.export_scalars_to_json(os.path.join(directory, 'logs', 'scalars.json'))
+    writer.close()
+
+    return best_model
+
+def main(args):
+    """
+    Train an autoencoder and save it
+    """
+
+    #Create directories if it don't exists
+    if not os.path.exists(args.directory):
+        os.makedirs(args.directory)
+    if not os.path.exists(os.path.join(args.directory, 'serial')):
+        os.makedirs(os.path.join(args.directory, 'serial'))
+    if not os.path.exists(os.path.join(args.directory, 'example_reconstruction')):
+        os.makedirs(os.path.join(args.directory, 'example_reconstruction'))
+    if not os.path.exists(os.path.join(args.directory, 'logs')):
+        os.makedirs(os.path.join(args.directory, 'logs'))
+
+    #Write arguments in a file
+    d = vars(args)
+    with open(os.path.join(args.directory, 'hyper-parameters'), 'w') as f:
+        for k in d.keys():
+            f.write('{}:{}\n'.format(k, d[k]))
+
+    #Variables
+    if args.random_projection == 0:
+        rp = False
+    else:
+        rp = True
+    ae = models.autoencoder.Autoencoder(args.nb_f, args.nb_l, args.nb_b, args.dense, rp, args.ips, args.dropout_rate)
+    ae = ae.cuda()
+    print(ae)
+    loss_function = torch.nn.MSELoss()
+    parameters = [p for p in ae.parameters() if p.requires_grad]
+    optimizer = torch.optim.Adam(parameters, args.learning_rate)
+
+    trainset = dataset.VideoDataset(args.trainset, args.root_dir)
+    testset = dataset.VideoDataset(args.testset, args.root_dir)
+
+    #Train the model and save it
+    best_model = train(ae, loss_function, optimizer, trainset, testset, args.epoch, args.batch_size, args.regularization, args.directory)
+    torch.save(best_model.state_dict(), os.path.join(args.directory, 'serial', 'best_model'))
+
+    return 0
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='')
+    #Training arguments
+    parser.add_argument('--trs', dest='trainset', type=str, default='data/umn_normal_trainset', help='Path to the trainset summary')
+    parser.add_argument('--tes', dest='testset', type=str, default='data/umn_testset', help='Path to the testset summary')
+    parser.add_argument('--rd', dest='root_dir', type=str, default='/datasets', help='Path to the images')
+    parser.add_argument('--bs', dest='batch_size', type=int, default=16, help='Mini batch size')
+    parser.add_argument('--lr', dest='learning_rate', type=float, default=0.0001, help='Learning rate')
+    parser.add_argument('--reg', dest='regularization', type=float, default=0.0001, help='Regularization')
+    parser.add_argument('--ep', dest='epoch', type=int, default=100, help='Number of training epochs')
+    parser.add_argument('--dir', dest='directory', type=str, default='train_autoencoder', help='Directory to store results')
+    #Model arguments
+    parser.add_argument('-f', dest='nb_f', type=int, default=16, help='Number of filters in the first downsampling block')
+    parser.add_argument('-l', dest='nb_l', type=int, default=1, help='Number of convolutinal layers per block')
+    parser.add_argument('-b', dest='nb_b', type=int, default=2, help='Number of upsampling blocks')
+    parser.add_argument('-d', dest='dense', type=int, default=None, help='Number of neurons in the middle denser layer (if None: no dense layer)')
+    parser.add_argument('-i', dest='ips', type=int, default=256, help='Image height (assume width = height)')
+    parser.add_argument('--do', dest='dropout_rate', type=float, default=0.5, help='Dropout rate')
+    parser.add_argument('-r', dest='random_projection', type=int, default=0, help='Random projection')
+    parser.add_argument('-p', dest='patch', type=int, default=32, help='Image patch size')
+    args = parser.parse_args()
+
+    main(args)
