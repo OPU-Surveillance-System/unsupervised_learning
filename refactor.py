5651d8178d888b16b5d468a15f4c34757a1d1508
henriojord <henriojord@gmail.com>
2018-03-03 22:55:01 +0900

    add missing import dataset

--- refactor.py
+++ refactor.py
@@ -98,8 +98,10 @@ def train(models, optimizers, datasets, epochs, batch_size, patch_size, z_dim, d
                     encoder.eval()
                     discriminator.zero_grad()
 
-                z_real = Variable(torch.randn(batch_size * ((256 // patch_size)**2), z_dim) * 5.).cuda()
+                z_real = Variable(torch.randn(batch_size, z_dim) * 5.).cuda()
+                print(z_real.shape)
                 z_fake = encoder(inputs)
+                print(z_fake.shape)
                 discriminator_real = discriminator(z_real)[1]
                 discriminator_fake = discriminator(z_fake)[1]
                 discriminator_loss = -torch.mean(torch.log(discriminator_real + tiny) + torch.log(1 - discriminator_fake + tiny))
@@ -110,6 +112,7 @@ def train(models, optimizers, datasets, epochs, batch_size, patch_size, z_dim, d
                 groundtruth = Variable(torch.cat((ones, zeros), 0)).cuda()
                 discriminator_out = torch.cat((discriminator_real, discriminator_fake), 0)
                 discriminator_out = (discriminator_out > 0.5)
+                print(discriminator_out.shape)
                 equality = (discriminator_out.float() == groundtruth)
                 discriminator_accuracy = equality.mean().data[0]
 
