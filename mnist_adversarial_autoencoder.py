afe36fbf63f4d6cdc64cbe875b3c636d873cbeef
henriojord <henriojord@gmail.com>
2018-03-03 14:55:23 +0900

    make encoder and decoder convolutional

--- mnist_adversarial_autoencoder.py
+++ mnist_adversarial_autoencoder.py
@@ -76,9 +76,9 @@ class Decoder(nn.Module):
         # self.lin3 = nn.Linear(N, X_dim)
         self.latent = nn.Linear(z_dim, f_dim*((28//2)**2))
         layers = []
+        layers.append(nn.Dropout2d(p=0.2))
         layers.append(nn.Upsample(scale_factor=2, mode='bilinear'))
         layers.append(nn.Conv2d(f_dim, 1, (3, 3), (1, 1), padding=1))
-        layers.append(nn.Dropout2d(p=0.2))
         self.conv = nn.Sequential(*layers)
 
     def forward(self, x):
