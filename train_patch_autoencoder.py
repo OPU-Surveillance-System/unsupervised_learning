5d00b2459cb52942288f6821814ff5c40ad8305f
henriojord <henriojord@gmail.com>
2018-03-10 16:10:06 +0900

    train by bce

--- train_patch_autoencoder.py
+++ train_patch_autoencoder.py
@@ -51,14 +51,16 @@ def train(model, loss_function, optimizer, trainset, testset, epoch, batch_size,
                 model.zero_grad()
                 inputs = Variable(sample['img'].float().cuda())
                 logits = model(inputs)
-                loss = loss_function(logits, inputs.view(-1, 3, args.patch, args.patch)) - reg * torch.mean(torch.norm(logits.view(-1, logits.size(1) * logits.size(2) * logits.size(3)), 2, 1))
+                loss = torch.nn.functional.binary_cross_entropy_with_logits(logits, inputs.view(-1, 3, patch_size, patch_size))
+                #loss = loss_function(logits, inputs.view(-1, 3, args.patch, args.patch)) - reg * torch.mean(torch.norm(logits.view(-1, logits.size(1) * logits.size(2) * logits.size(3)), 2, 1))
                 if p == 'train':
                     loss.backward()
                     optimizer.step()
                 running_loss += loss.data[0]
                 if p == 'test':
                     logits = logits.view(-1, 3, args.ips, args.ips)
-                    tmp = utils.metrics.per_image_error(dist, logits, inputs)
+                    #tmp = utils.metrics.per_image_error(dist, logits, inputs)
+                    tmp = utils.metrics.per_image_error(dist, torch.nn.functional.sigmoid(logits), inputs)
                     errors += tmp.data.cpu().numpy().tolist()
                     labels += sample['lbl'].numpy().tolist()
 
@@ -80,7 +82,8 @@ def train(model, loss_function, optimizer, trainset, testset, epoch, batch_size,
                     torch.save(model.state_dict(), os.path.join(directory, 'serial', 'model_{}'.format(e)))
 
                     #Plot example of reconstructed images
-                    pred = utils.process.deprocess(logits)
+                    #pred = utils.process.deprocess(logits)
+                    pred = torch.nn.functional.sigmoid(logits)
                     pred = pred.data.cpu().numpy()
                     pred = np.rollaxis(pred, 1, 4)
                     inputs = utils.process.deprocess(inputs)
