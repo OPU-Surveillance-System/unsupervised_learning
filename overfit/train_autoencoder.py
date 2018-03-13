af924686e251fe70f85e4212d471f3cec02d01e2
henriojord <henriojord@eisti.eu>
2018-03-13 16:15:11 +0900

    try to make plain autoencoder overfit

--- overfit/train_autoencoder.py
+++ overfit/train_autoencoder.py
@@ -86,7 +86,7 @@ def train(model, optimizer, trainset, testset, epoch, batch_size, directory):
                 inputs = utils.process.deprocess(inputs)
                 inputs = inputs.data.cpu().numpy()
                 inputs = np.rollaxis(inputs, 1, 4)
-                utils.plot.plot_reconstruction_images(inputs, pred, os.path.join(directory, 'example_reconstruction_{}'.format(e), 'epoch_{}.svg'.format(e)))
+                utils.plot.plot_reconstruction_images(inputs, pred, os.path.join(directory, 'example_reconstruction_{}'.format(p), 'epoch_{}.svg'.format(e)))
     writer.export_scalars_to_json(os.path.join(directory, 'logs', 'scalars.json'))
     writer.close()
 
