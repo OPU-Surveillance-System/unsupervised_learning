5287dcfccd5b4f2944c2399f0ca3aa2b3b547236
henriojord <henriojord@eisti.eu>
2018-03-13 22:41:06 +0900

    rm mean

--- overfit/train_variational_autoencoder.py
+++ overfit/train_variational_autoencoder.py
@@ -69,7 +69,7 @@ def train(models, optimizers, trainset, testset, epoch, batch_size, reg, directo
                 z = sample_z(mu, sigma)
                 logits = decoder(z)
                 reconstruction_loss = torch.nn.functional.mse_loss(logits, inputs)
-                regularization_loss = 0.5 * torch.sum(torch.exp(sigma) + mu**2 - 1. - sigma, 1)
+                regularization_loss = 0.5 * torch.sum(torch.exp(sigma) + mu**2 - 1. - sigma)
                 loss = reconstruction_loss + reg * regularization_loss
                 if p == 'train':
                     loss.backward()
