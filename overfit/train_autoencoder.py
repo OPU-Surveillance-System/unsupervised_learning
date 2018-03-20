feb6da5e4f2d7387e925499533c3e447fe82889b
henriojord <henriojord@eisti.eu>
2018-03-20 10:41:04 +0900

    add possibility to use greyscale images

--- overfit/train_autoencoder.py
+++ overfit/train_autoencoder.py
@@ -93,6 +93,7 @@ def train(model, optimizer, trainset, testset, epoch, batch_size, noise_ratio, d
                 groundtruth = utils.process.deprocess(groundtruth)
                 groundtruth = groundtruth.data.cpu().numpy()
                 groundtruth = np.rollaxis(groundtruth, 1, 4)
+                print(groundtruth.shape)
                 if groundtruth.shape[4] == 1:
                     pred = pred.reshape((-1, 256, 256))
                     inputs = inputs.reshape((-1, 256, 256))
