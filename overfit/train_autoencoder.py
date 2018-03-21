97040561d0fbecfb99fc2268e4fc0e67ab90ed06
henriojord <henriojord@gmail.com>
2018-03-21 13:31:08 +0900

    no dropout pixels

--- overfit/train_autoencoder.py
+++ overfit/train_autoencoder.py
@@ -52,9 +52,8 @@ def train(model, optimizer, trainset, testset, epoch, batch_size, noise_ratio, d
                 inputs = Variable(sample['img'].float().cuda())
                 groundtruth = Variable(sample['img'].float().cuda())
                 if p == 'train':
-                    # noise =  Variable(torch.randn(inputs.size()) * noise_ratio).float().cuda()
-                    # inputs = inputs + noise
-                    inputs = torch.nn.functional.dropout2d(inputs, p=noise_ratio)
+                    noise =  Variable(torch.randn(inputs.size()) * noise_ratio).float().cuda()
+                    inputs = inputs + noise
                 logits = model(inputs)
                 loss = torch.nn.functional.mse_loss(logits, inputs)
                 if p == 'train':
