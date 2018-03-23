fc5ae9cf839883bd991600b9f578feb3c1b6081b
henriojord <henriojord@eisti.eu>
2018-03-23 15:40:08 +0900

    plot reconstruction

--- autoregressive/train_pcnn.py
+++ autoregressive/train_pcnn.py
@@ -53,20 +53,20 @@ def train(pcnn, optimizer, trainset, testset, epoch, batch_size, directory):
             writer.add_scalar('learning_curve/{}'.format(p), epoch_loss, e)
             print('Epoch {} ({}): loss = {}'.format(e, p, epoch_loss))
 
-            if p == 'test':
-                synthetic = torch.zeros(16, 1, 28, 28).cuda()
-                for i in tqdm(range(28)):
-                    for j in range(28):
-                        probs = pcnn(Variable(synthetic, volatile=True))[0]
-                        probs = torch.nn.functional.softmax(probs[:, :, i, j]).data
-                        synthetic[:, :, i, j] = torch.multinomial(probs, 1).float() / 255.
-
-                synthetic = synthetic.cpu().numpy()
-                synthetic = np.moveaxis(synthetic, 1, -1)
-                synthetic = np.reshape(synthetic, (4 * 28, 4 * 28))
-                plt.clf()
-                plt.imshow(synthetic)
-                plt.savefig(os.path.join(directory, 'generation', '{}.svg'.format(e)), format='svg', bbox_inches='tight')
+            # if p == 'test':
+            #     synthetic = torch.zeros(16, 1, 28, 28).cuda()
+            #     for i in tqdm(range(28)):
+            #         for j in range(28):
+            #             probs = pcnn(Variable(synthetic, volatile=True))[0]
+            #             probs = torch.nn.functional.softmax(probs[:, :, i, j]).data
+            #             synthetic[:, :, i, j] = torch.multinomial(probs, 1).float() / 255.
+            #
+            #     synthetic = synthetic.cpu().numpy()
+            #     synthetic = np.moveaxis(synthetic, 1, -1)
+            #     synthetic = np.reshape(synthetic, (4 * 28, 4 * 28))
+            #     plt.clf()
+            #     plt.imshow(synthetic)
+            #     plt.savefig(os.path.join(directory, 'generation', '{}.svg'.format(e)), format='svg', bbox_inches='tight')
 
             if p == 'train':
                 logits = logits.permute(0, 2, 3, 1)
