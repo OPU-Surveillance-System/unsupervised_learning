921016c7370ea0ce3716e0b40aca13a3a78fc6f6
henriojord <henriojord@gmail.com>
2018-03-24 10:00:34 +0900

    add batchnorm

--- autoregressive/pixelcnn.py
+++ autoregressive/pixelcnn.py
@@ -35,10 +35,13 @@ class ResidualBlock(torch.nn.Module):
     layers = []
     layers.append(torch.nn.ReLU())
     layers.append(torch.nn.Conv2d(2 * self.h, self.h, (1, 1)))
+    layers.append(torch.nn.BatchNorma2d(self.h))
     layers.append(torch.nn.ReLU())
     layers.append(MaskedConvolution(self.h, self.h, (3, 3), 'B', 1))
+    layers.append(torch.nn.BatchNorma2d(self.h))
     layers.append(torch.nn.ReLU())
     layers.append(torch.nn.Conv2d(self.h, 2 * self.h, (1, 1)))
+    layers.append(torch.nn.BatchNorma2d(2 * self.h))
     self.layers = torch.nn.Sequential(*layers)
 
     # #Weights initialization
@@ -61,8 +64,9 @@ class PixelCNN(torch.nn.Module):
     self.d = d
 
     self.first_layer = MaskedConvolution(1, 2 * self.h, (7, 7), 'A', 3)
+    self.bn = torch.nn.BatchNorma2d(2 * self.h)
     self.residual_blocks = torch.nn.Sequential(*[ResidualBlock(self.h) for n in range(self.n + 1)])
-    self.top_layer = torch.nn.Sequential(*[torch.nn.ReLU(), torch.nn.Conv2d(2 * self.h, self.d, (1, 1)), torch.nn.ReLU()])
+    self.top_layer = torch.nn.Sequential(*[torch.nn.ReLU(), torch.nn.Conv2d(2 * self.h, self.d, (1, 1)), torch.nn.BatchNorma2d(self.d), torch.nn.ReLU()])
     self.evidence = torch.nn.Conv2d(self.d, 256, (1, 1))
 
     # #Weights initialization
