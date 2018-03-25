58201056b25e2fc6b407cb6d8d3e0a0ef6aaf544
henriojord <henriojord@gmail.com>
2018-03-25 11:42:47 +0900

    add distance auc

--- autoregressive/train_pcnn.py
+++ autoregressive/train_pcnn.py
@@ -50,6 +50,7 @@ def train(pcnn, optimizer, trainset, testset, epoch, batch_size, ims, directory)
                 if p == 'test':
                     probs = torch.nn.functional.softmax(logits, dim=3)
                     argmax = torch.max(probs, 3)[1]
+                    print(argmax.shape, lbl.shape)
                     tmp = utils.metrics.per_image_error(dist, argmax, lbl)
                     errors += tmp.data.cpu().numpy().tolist()
                     labels += sample['lbl'].numpy().tolist()
