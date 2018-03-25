fa84bb32ea627d1daf31d5cec9b07fd31e653af1
henriojord <henriojord@gmail.com>
2018-03-25 11:02:23 +0900

    add tester

--- autoregressive/test_pcnn.py
+++ autoregressive/test_pcnn.py
@@ -44,6 +44,7 @@ def test(pcnn, testset, batch_size, directory):
         print('merge', merge.shape)
         merge = torch.sum(merge, 1)
         merge = torch.unsqueeze(merge, 1)
+        print(merge[0][0])
         print('sum', merge.shape)
         prob = torch.prod(merge, 1)
         print('prod', prob.shape, prob)
