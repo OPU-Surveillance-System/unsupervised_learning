8db59fa1d3edf7331f802366820c49e3dadb1db9
henriojord <henriojord@gmail.com>
2018-03-25 10:54:41 +0900

    add tester

--- autoregressive/test_pcnn.py
+++ autoregressive/test_pcnn.py
@@ -37,7 +37,7 @@ def test(pcnn, testset, batch_size, directory):
         print('lbl', lbl.shape)
         output = pcnn(img)[1]
         print('output', output.shape)
-        onehot_lbl = torch.FloatTensor(batch_size, 256, 64, 64).zero_()
+        onehot_lbl = torch.FloatTensor(batch_size, 256, 64, 64).zero_().cuda()
         onehot_lbl = onehot_lbl.scatter_(1, lbl.data, 1)
         print('onehot', onehot_lbl.shape)
         merge = output * onehot_lbl
