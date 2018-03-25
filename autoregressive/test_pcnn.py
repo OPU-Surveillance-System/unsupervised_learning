67c079d7f94fb8d2f4bacf2d5527f5251fa0b191
henriojord <henriojord@gmail.com>
2018-03-25 10:35:05 +0900

    add tester

--- autoregressive/test_pcnn.py
+++ autoregressive/test_pcnn.py
@@ -33,6 +33,7 @@ def test(pcnn, testset, batch_size, directory):
     for i_batch, sample in enumerate(tqdm(dataloader)):
         img = Variable(sample['img'], volatile=True).float().cuda()
         lbl = Variable(img.data[:, 0] * 255, volatile=True).long().cuda()
+        print('lbl', lbl.shape)
         output = pcnn(img)[1]
         print('output', output.shape)
         onehot_lbl = lbl.scatter_(1, lbl, 1)
