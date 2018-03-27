a06e1c8f522669e952a3be360ea3b356a68d96c8
henriojord <henriojord@eisti.eu>
2018-03-27 13:25:14 +0900

    add test pcnn mnsit

--- autoregressive/test_pcnn_mnist.py
+++ autoregressive/test_pcnn_mnist.py
@@ -41,7 +41,7 @@ def test(pcnn, testset, batch_size, directory):
                 masked = Variable(torch.zeros(img.size(0), 1, 28, 28).cuda())
                 print(img.shape)
                 masked[:, :, 0:i+1, 0:j+1] = img[:, :, 0:i+1, 0:j+1]
-                masked = masked.cpu().numpy()
+                masked = masked.data.cpu().numpy()
                 plt.clf()
                 plt.imshow(masked[0].reshape((28, 28)))
                 plt.savefig(os.path.join(directory, 'plots', '{}_{}.svg'.format(i, j)), format='svg', bbox_inches='tight')
