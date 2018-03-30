d5aa4a210ca7e547ce08103e1b7f0b001993b000
henriojord <henriojord@eisti.eu>
2018-03-30 13:30:18 +0900

    plot hist

--- utils/plot.py
+++ utils/plot.py
@@ -56,8 +56,12 @@ def plot_likelihood_hist(n, a, name=None):
     """
 
     plt.clf()
-    plt.hist(n, bins=100, alpha=0.5, normed=1, label='MNIST', color='blue')
-    plt.hist(a, bins=100, alpha=0.5, normed=1, label='Alphabet', color='red')
+    x, bins, p = plt.hist(n, bins=100, alpha=0.5, label='MNIST', color='blue')
+    for item in p:
+        item.set_height(item.get_height()/sum(x))
+    x, bins, p = plt.hist(a, bins=100, alpha=0.5, label='Alphabet', color='red')
+    for item in p:
+        item.set_height(item.get_height()/sum(x))
     plt.xlabel('Log likelihood')
     plt.ylabel('Number of images')
     plt.legend(loc='upper right')
