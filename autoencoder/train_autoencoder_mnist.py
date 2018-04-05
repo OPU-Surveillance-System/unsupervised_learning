2d7f3b1df662bb149fb7372bb6504e3ebf9aa948
henriojord <henriojord@eisti.eu>
2018-04-05 14:53:26 +0900

    fix autoencoder

--- autoencoder/train_autoencoder_mnist.py
+++ autoencoder/train_autoencoder_mnist.py
@@ -82,7 +82,7 @@ for e in range(args.epoch):
         for i_batch, sample in enumerate(tqdm(dataloader)):
             optimizer.zero_grad()
 
-            inputs = Variable(sample['img'].float().cuda())
+            inputs = Variable(sample[0].float().cuda())
             logits = ae(inputs)
             loss = torch.nn.functional.mse_loss(logits, inputs)
             running_loss += loss.data[0]
@@ -104,7 +104,7 @@ for e in range(args.epoch):
             items = {}
             #Process the testset
             for i_batch, sample in enumerate(tqdm(dataloader)):
-                inputs = Variable(sample['img'].float().cuda())
+                inputs = Variable(sample[0].float().cuda())
                 logits = ae(inputs)
 
                 tmp = utils.metrics.per_image_error(dist, logits, inputs)
