da5566e372c04f5265cb74f5d603660002dfa9cf
henriojord <henriojord@eisti.eu>
2018-04-27 13:50:02 +0900

    avenue17

--- data/processing/avenue.py
+++ data/processing/avenue.py
@@ -15,7 +15,8 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('-d', dest='dataset', type=str, default='/home/scom/Downloads/Avenue Dataset', help='Path to the avenue dataset')
 parser.add_argument('-t', dest='target', type=str, default='/home/scom/avenue64', help='Directory to store resulting frames')
-parser.add_argument('-r', dest='resize', default='64,64', help='Specify the size at which the frames should be resized (format: heigt,width)')
+parser.add_argument('-r', dest='resize', type=str, default='64,64', help='Specify the size at which the frames should be resized (format: heigt,width)')
+parser.add_argument('-s', dest='avenue17', type=int, default=0, help='Avenue17 (0 or 1)')
 
 args = parser.parse_args()
 
@@ -31,15 +32,22 @@ if args.resize is not '':
 else:
     resize = None
 
+if args.avenue17 == 1:
+    addon = '17'
+else:
+    addon = ''
+
+avenue17 = ['01', '02', '08', '09', '10']
+
 print('Process train set')
-with open(os.path.join(args.target, 'train', 'avenue_trainset'), 'w') as fi: #Empty summary file
+with open(os.path.join(args.target, 'avenue_trainset'), 'w') as fi: #Empty summary file
     pass
 files = sorted([os.path.join(os.path.join(args.dataset, 'training_videos', f)) for f in os.listdir(os.path.join(args.dataset, 'training_videos'))])
 for f in tqdm(files):
     v = imageio.get_reader(f, 'ffmpeg')
     nb_frames = v.get_meta_data()['nframes']
     filename = ntpath.basename(f)[:-4]
-    with open(os.path.join(args.target, 'train', 'avenue_trainset'), 'a') as fi:
+    with open(os.path.join(args.target, 'avenue_trainset'), 'a') as fi:
         for frame in range(nb_frames):
             try: #Sometimes a RuntimeError occurs while fetching the last frame
                 f_data = v.get_data(frame)
@@ -52,15 +60,17 @@ for f in tqdm(files):
             fi.write('{}_{}.png\t1\n'.format(os.path.join('train', filename), frame))
 
 print('Process test set')
-with open(os.path.join(args.target, 'test', 'avenue_testset'), 'w') as fi:
+with open(os.path.join(args.target, 'avenue_testset' + addon), 'w') as fi:
     pass
 files = sorted([os.path.join(os.path.join(args.dataset, 'testing_videos', f)) for f in os.listdir(os.path.join(args.dataset, 'testing_videos'))])
 for f in tqdm(files):
+    if args.avenue17 == 1 and any(clip in f for clip in avenue17):
+        continue
     v = imageio.get_reader(f, 'ffmpeg')
     nb_frames = v.get_meta_data()['nframes']
     filename = ntpath.basename(f)[:-4]
     labels = io.loadmat(os.path.join(args.dataset, 'ground_truth_demo', 'testing_label_mask', '{}_label.mat'.format(int(filename))))['volLabel'][0]
-    with open(os.path.join(args.target, 'test', 'avenue_testset'), 'a') as fi:
+    with open(os.path.join(args.target, 'avenue_testset' + addon), 'a') as fi:
         for frame in range(nb_frames):
             try: #Sometimes a RuntimeError occurs while fetching the last frame
                 f_data = v.get_data(frame)
