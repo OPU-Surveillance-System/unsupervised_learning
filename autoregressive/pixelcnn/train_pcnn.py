fb03f484dbfe3442f91a04c60cda86c7eb886f27
henriojord <henriojord@eisti.eu>
2018-05-14 11:27:05 +0900

    add entropy regularization

--- autoregressive/pixelcnn/train_pcnn.py
+++ autoregressive/pixelcnn/train_pcnn.py
@@ -20,7 +20,7 @@ def compute_entropy(logits):
     probs = torch.nn.functional.softmax(logits, 1)
     entropy = -torch.sum(probs * torch.log(probs), 1)
     mean_entropy = entropy.mean()
-    if mean_entropy != mean_entropy:
+    if mean_entropy.cpu().numpy().isnan():
         print('Mean entropy is NaN')
         print(probs)
         print(entropy)
